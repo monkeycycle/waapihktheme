1bb19fcaf595fbd5bcd00ff3fe1ce91d4b70f7ae
Michael Pereira <monkeycycle@gmail.com>
2022-12-05 12:45:08 -0600

    removed sample plot code

--- R/theme_waapihk.R
+++ R/theme_waapihk.R
@@ -124,87 +124,57 @@ scale_fill_waapihk <- function(palette = 'main', discrete = TRUE, reverse = FALS
 #'
 #' @export
 
-theme_waapihk <- function(
-    base_font_size=18,
-    base_font_family="Open Sans",
-    position = 'center'
-) {
+theme_waapihk <- function(base_size = 18,
+                          base_family = "Open Sans") {
+  font <- base_family
 
   ggplot2::theme(
-    plot.margin = ggplot2::margin(t = 15, r = 15, b = 15, l = 15, unit = 'pt'),
-    # plot.margin=unit(c(.5,.8,0,.8),"cm"),
-    plot.background=ggplot2::element_rect( fill="#ffffff", colour=NA ),
-    plot.title = ggplot2::element_text(family=base_font_family, size=base_font_size + 8, lineheight=1.2, face="bold", color="#222222", margin=ggplot2::margin(0,0,0,0)),
-    plot.title.position = 'plot',
-    plot.subtitle = ggplot2::element_text(family=base_font_family, size=base_font_size - 2, lineheight=1, margin=ggplot2::margin(5,0,20,0)),
-    plot.caption = ggplot2::element_text(family=base_font_family, size=base_font_size - 6, lineheight=1.2, color="#222222", margin=ggplot2::margin(0,0,0,0)),
-    plot.caption.position = 'plot',
-
-    #This leaves the caption text element empty, because it is set elsewhere in the finalize plot function
-    # plot.caption = ggplot2::element_blank(),
+    plot.margin = unit(c(.5, .8, 0, .8), "cm"),
+    plot.background = ggplot2::element_rect(fill = "#ffffff", colour = NA),
+    plot.title = ggplot2::element_text(family = font, size = 24, lineheight = 1.2, face = "bold", color = "#222222", margin = ggplot2::margin(0, 0, 0, 0)),
+    plot.subtitle = ggplot2::element_text(family = font, size = 16, lineheight = 1, margin = ggplot2::margin(5, 0, 10, 0)),
+    # This leaves the caption text element empty, because it is set elsewhere in the finalize plot function
+    plot.caption = ggplot2::element_blank(),
 
 
 
-    #Legend format
-    #This sets the position and alignment of the legend, removes a title and backround for it and sets the requirements for any text within the legend. The legend may often need some more manual tweaking when it comes to its exact position based on the plot coordinates.
-    legend.background = ggplot2::element_blank(),
-    legend.key = element_rect(fill="#ffffff", colour=NA),
-    legend.key.width=unit(3, "mm"),
-    legend.key.height=unit(3, "mm"),
-    legend.position = 'top',
-    legend.direction = 'horizontal',
-    legend.justification = 'top',
-    legend.box = 'vertical',
-    legend.margin = ggplot2::margin(0, 0, 0, 0),
-
-    legend.title = ggplot2::element_blank(),
-    legend.text=ggplot2::element_text(family=base_font_family, size=base_font_size - 4, color="#222222", lineheight=1.2),
+    # Legend format
+    # This sets the position and alignment of the legend, removes a title and backround for it and sets the requirements for any text within the legend. The legend may often need some more manual tweaking when it comes to its exact position based on the plot coordinates.
+    legend.key = element_rect(fill = "transparent", colour = NA),
+    legend.key.width = unit(3, "mm"),
+    legend.key.height = unit(3, "mm"),
+    legend.position = "top",
     legend.text.align = 0,
+    legend.background = ggplot2::element_blank(),
+    legend.title = ggplot2::element_blank(),
+    legend.text = ggplot2::element_text(family = font, size = 14, color = "#222222", lineheight = 1.2),
 
 
-    #Axis format
-    #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
-    # axis.title=ggplot2::element_text(ggplot2::margin(0, 0, 0, 0), family=base_font_family, size=base_font_size - 4, color="#000000"),
-    # axis.text=ggplot2::element_text(family=base_font_family, size=, color="#000000"),
-    # axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
-    axis.title=ggplot2::element_text(family=base_font_family, size=base_font_size - 4, color="#000000"),
-    axis.text=ggplot2::element_text(family=base_font_family, size=base_font_size - 8, color="#222222"),
-    axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
-    axis.ticks = ggplot2::element_line(color="#d0d0d0", size = 0.5),
-    axis.line = ggplot2::element_line(color="#d0d0d0", size = 0.5),
+
+    # Axis format
+    # This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
+    axis.title = ggplot2::element_text(family = font, size = 12, color = "#000000"),
+    axis.text = ggplot2::element_text(family = font, size = 12, color = "#222222"),
+    axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10)),
+    axis.ticks = ggplot2::element_line(color = "#777777"),
+    axis.line = ggplot2::element_line(color = "#777777"),
     # axis.line.x=ggplot2::element_blank(),
     # axis.line.y=ggplot2::element_blank(),
 
-    # Axis stuff
-    # axis.text = ggplot2::element_text( size = , color = 'black'),
-    # axis.title = ggplot2::element_blank(),
-    # axis.line.x = ggplot2::element_blank(),
-    # axis.ticks.y = ggplot2::element_blank(),
-    # axis.ticks.x = ggplot2::element_line(colour = 'black', size = 0.5),
-    # axis.ticks.length = ggplot2::unit(5, 'pt'),
-
-
-
-    #Grid lines
+    # Grid lines
     panel.grid.major = ggplot2::element_blank(),
     # panel.grid.major.x = ggplot2::element_blank(),
-    # panel.grid.major.y = ggplot2::element_line(color = '#d0d0d0', size = 0.5),
-    # panel.grid.major.y = ggplot2::element_line(color = '#d0d0d0', size = 0.5),
+    # panel.grid.major.y = ggplot2::element_line(color="#cbcbcb"),
     panel.grid.minor = ggplot2::element_blank(),
-    # panel.grid.minor.x = ggplot2::element_line(color="#cbcbcb", size = 0.5),
-    # panel.grid.minor.y = ggplot2::element_line(color="#cbcbcb", size = 0.5),
-    panel.spacing = ggplot2::unit(base_font_size - 4, 'pt'),
+    # panel.grid.minor.x = ggplot2::element_blank(),
+    # panel.grid.minor.y = ggplot2::element_line(color="#cbcbcb"),
+    panel.spacing = unit(2, "lines"),
     panel.border = ggplot2::element_blank(),
-    panel.background = ggplot2::element_rect(fill="#ffffff"),
+    panel.background = ggplot2::element_rect(fill = "#ffffff"),
 
-
-    # Facets
-    strip.background = ggplot2::element_rect(fill="#ffffff"),
-    strip.placement = 'outside',
-    strip.text = ggplot2::element_text(family=base_font_family, face="bold", size = base_font_size - 4, margin=ggplot2::margin(5,0,10,0), hjust=0)
-
-
+    # Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
+    strip.background = ggplot2::element_rect(fill = "#ffffff"),
+    strip.text = ggplot2::element_text(family = font, face = "bold", color = "#555555", size = 14, margin = ggplot2::margin(5, 0, 10, 0), hjust = 0)
   )
 }
-
 
